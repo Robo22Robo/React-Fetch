6b90d1cdfa0122e481e9b71fa025797f5b595022
DCI-E0345 <robo22robo@yahoo.de>
2019-12-17 15:46:22 +0100

    Updates

---
+++ precache-manifest.b9ee7b41e86a2ad9db60e404a334992d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7c65ce602fa88be9c82bf8d1f0e2e3",
+    "url": "/React-Fetch/index.html"
+  },
+  {
+    "revision": "b94b751ccdb1c7aae65f",
+    "url": "/React-Fetch/static/css/main.c27d0827.chunk.css"
+  },
+  {
+    "revision": "7ce6bfafc785fcbc5b4a",
+    "url": "/React-Fetch/static/js/2.3b577150.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Fetch/static/js/2.3b577150.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b94b751ccdb1c7aae65f",
+    "url": "/React-Fetch/static/js/main.c980587a.chunk.js"
+  },
+  {
+    "revision": "167ed429099617d23e3c",
+    "url": "/React-Fetch/static/js/runtime-main.c30ada30.js"
+  }
+]);
